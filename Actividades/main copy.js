9e9d36978b7c2fa4aadf09d1cbe5256676e154cb
valemicolgarcia <valemicolgarcia@gmail.com>
2024-12-09 20:37:16 -0300

    actividades a completar

--- Actividades/main copy.js
+++ Actividades/main copy.js
@@ -26,12 +26,21 @@ Debe contar con un método “getProductById” el cual debe buscar en el arregl
 En caso de no coincidir ningún id, mostrar en consola un error “Not found”
 
 */
+
+const fs = requiere("fs").promises;
+//con ESModules: import {promises as fs} from "fs"
+
 class ProductManager {
     static ultId = 0; //pertencen a la clase y no requeiren instancia
     constructor(products) {
         this.products = []; //arreglo vacio
     }
-    addProduct(title, description, price, img, code, stock) {
+    async addProduct(title, description, price, img, code, stock) {
+
+        //yo puedo leer el archivo y guardarme el array con los productos:
+        const arrayProductos = await this.leerArchivo();
+
+
         //validamos que se agregaron todos los campos
         if (!title || !description || !price || !img || !code || !stock) {
             console.log("Todos los campos son obligatorios, falta alguno!");
@@ -56,7 +65,7 @@ class ProductManager {
         }
 
         //una vez que lo puedo crear lo pusheo al array
-        this.products.push(nuevoProducto);
+        arrayProductos.push(nuevoProducto);
 
 
     }
@@ -75,11 +84,23 @@ class ProductManager {
     }
 
     //armar metodos auxiliares que guarden el archivo y recuperen los datos
-    async guardarArchivo() {
+    async guardarArchivo(arrayProductos) {
         try {
-
+            await fs.writeFile(this.path, JSON.stringify(arrayProductos, null, 2))
         } catch (error) {
             console.log("Tenemos un error al guardar el archivo");
+        }
+    }
+
+    async leerArchivo() {
+        try {
+
+            const respuesta = await fs.readFile(this.path, "utf-8");
+            const arrayProductos = JSON.parse(respuesta);
+            return arrayProductos;
+
+        } catch (error) {
+            console.log("Tenemos un error al leer el archivo")
         }
     }
 
