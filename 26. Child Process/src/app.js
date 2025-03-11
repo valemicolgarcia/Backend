b14262333a47a2bdf8781b99ad1a46f4f4fda92f
valemicolgarcia <valemicolgarcia@gmail.com>
2025-03-10 21:46:59 -0300

    child process

---
+++ 26. Child Process/src/app.js
@@ -0,0 +1,31 @@
+//CHILD PROCESS
+//Tecnica que vamos a utilizar: fork
+
+import express from "express";
+const app = express();
+const PUERTO = 8080;
+
+app.get("/", (req, res) => {
+  res.send("hola");
+});
+
+import { fork } from "child_process";
+//const { fork } = require("child_process");
+//no hace falta instalar nada, es un proceso nativo de node
+
+app.get("/suma", (req, res) => {
+  const child = fork("./src/operacionesComplejas.js");
+  child.send("iniciando");
+  child.once("message", (mensaje) => {
+    res.send(`el resultado de la operacion es: ${mensaje}`);
+  });
+});
+
+app.listen(PUERTO, () => console.log(`Estamos bien`));
+
+//tenemos que lograr que el proceso de suma se realiza sin bloquear al resto de los endpoints
+//comenzamos con la estructura del forkeo
+
+//1. separamos la funcion que trae problemas a otro modulo.
+//2. la modificamos y la dejamos disponible para cuando el padre la solicite
+//3. ejecutamos la ruta
