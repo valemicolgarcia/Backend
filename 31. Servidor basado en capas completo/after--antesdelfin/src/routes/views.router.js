9a66bdced443efb89ac9a11d2fc0add6048b05d1
valemicolgarcia <valemicolgarcia@gmail.com>
2025-04-01 10:07:12 -0300

    middlewares

--- 31. Servidor basado en capas completo/after--antesdelfin/src/routes/views.router.js
+++ 31. Servidor basado en capas completo/after--antesdelfin/src/routes/views.router.js
@@ -8,73 +8,84 @@ const cartManager = new CartManager();
 
 import passport from "passport";
 
-router.get("/products", passport.authenticate("jwt", {session: false}) ,async (req, res) => {
-   try {
+//me traigo los middlewares de autorizacion
+import { soloAdmin, soloUser } from "../middleware/auth.js";
+
+router.get(
+  "/products",
+  passport.authenticate("jwt", { session: false }),
+  soloUser,
+  async (req, res) => {
+    try {
       const { page = 1, limit = 2 } = req.query;
       const productos = await productManager.getProducts({
-         page: parseInt(page),
-         limit: parseInt(limit)
+        page: parseInt(page),
+        limit: parseInt(limit),
       });
 
-      const nuevoArray = productos.docs.map(producto => {
-         const { _id, ...rest } = producto.toObject();
-         return rest;
+      const nuevoArray = productos.docs.map((producto) => {
+        const { _id, ...rest } = producto.toObject();
+        return rest;
       });
 
       res.render("products", {
-         productos: nuevoArray,
-         hasPrevPage: productos.hasPrevPage,
-         hasNextPage: productos.hasNextPage,
-         prevPage: productos.prevPage,
-         nextPage: productos.nextPage,
-         currentPage: productos.page,
-         totalPages: productos.totalPages
+        productos: nuevoArray,
+        hasPrevPage: productos.hasPrevPage,
+        hasNextPage: productos.hasNextPage,
+        prevPage: productos.prevPage,
+        nextPage: productos.nextPage,
+        currentPage: productos.page,
+        totalPages: productos.totalPages,
       });
-
-   } catch (error) {
+    } catch (error) {
       console.error("Error al obtener productos", error);
       res.status(500).json({
-         status: 'error',
-         error: "Error interno del servidor"
+        status: "error",
+        error: "Error interno del servidor",
       });
-   }
-});
+    }
+  }
+);
 
 router.get("/carts/:cid", async (req, res) => {
-   const cartId = req.params.cid;
+  const cartId = req.params.cid;
 
-   try {
-      const carrito = await cartManager.getCarritoById(cartId);
+  try {
+    const carrito = await cartManager.getCarritoById(cartId);
 
-      if (!carrito) {
-         console.log("No existe ese carrito con el id");
-         return res.status(404).json({ error: "Carrito no encontrado" });
-      }
+    if (!carrito) {
+      console.log("No existe ese carrito con el id");
+      return res.status(404).json({ error: "Carrito no encontrado" });
+    }
 
-      const productosEnCarrito = carrito.products.map(item => ({
-         product: item.product.toObject(),
-         //Lo convertimos a objeto para pasar las restricciones de Exp Handlebars. 
-         quantity: item.quantity
-      }));
+    const productosEnCarrito = carrito.products.map((item) => ({
+      product: item.product.toObject(),
+      //Lo convertimos a objeto para pasar las restricciones de Exp Handlebars.
+      quantity: item.quantity,
+    }));
 
-
-      res.render("carts", { productos: productosEnCarrito });
-   } catch (error) {
-      console.error("Error al obtener el carrito", error);
-      res.status(500).json({ error: "Error interno del servidor" });
-   }
+    res.render("carts", { productos: productosEnCarrito });
+  } catch (error) {
+    console.error("Error al obtener el carrito", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
 });
 
 router.get("/login", (req, res) => {
-    res.render("login"); 
-})
+  res.render("login");
+});
 
 router.get("/register", (req, res) => {
-    res.render("register"); 
-})
+  res.render("register");
+});
 
-router.get("/realtimeproducts",passport.authenticate("jwt", {session: false}),(req, res) => {
-   res.render("realtimeproducts"); 
-})
+router.get(
+  "/realtimeproducts",
+  passport.authenticate("jwt", { session: false }),
+  soloAdmin,
+  (req, res) => {
+    res.render("realtimeproducts");
+  }
+);
 
 export default router;
